0a3c3defeffb2cb5c5862e4db5602fcd01932033
Tanim1440 <mtanimkhan520@gmail.com>
2025-12-18 23:12:36 +0600

    fixied app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
-import React from "react";
+// import React from "react";
+// import Navber from "./components/Navber";
+// import { Routes, useLocation } from "react-router-dom";
+// import Home from "./components/Home";
+
+
+// const App = () => {
+
+//   const isOwnerPath=useLocation().pathname.includes("owner");
+
+//   return (
+//     <div>
+//       {!isOwnerPath && <Navber/>}
+//       <div className='min-h-[70vh]'>
+//        <Routes>
+//         <Route path='/' element={<Home/>}/>
+//        </Routes>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navber from "./components/Navber";
-import { Routes } from "react-router-dom";
 import Home from "./components/Home";
 
-
 const App = () => {
-
-  const isOwnerPath=useLocation().pathname.includes("owner");
+  const location = useLocation();
+  const isOwnerPath = location.pathname.includes("owner");
 
   return (
     <div>
-      {!isOwnerPath && <Navber/>}
-      <div className='min-h-[70vh]'>
-       <Routes>
-        <Route path='/' element={<Home/>}/>
-       </Routes>
-      </div>
-    </div>
-  )
-}
+      {!isOwnerPath && <Navber />}
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
